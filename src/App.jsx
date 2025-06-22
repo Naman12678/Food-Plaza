6bc4012a2cd452f5f6b86ef2a47654ee6a2e3e8c
Naman Sharma <namansharma12678@gmail.com>
2025-06-22 23:38:38 +0530

    Add @vercel/analytics for enhanced analytics tracking in the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RecipeSearch from './components/RecipeSearch';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
+    <>
     <Router>
       <div className="App">
         <Routes>
@@ -13,6 +15,8 @@ const App = () => {
         </Routes>
       </div>
     </Router>
+    <Analytics/>
+    </>
   );
 };
 
